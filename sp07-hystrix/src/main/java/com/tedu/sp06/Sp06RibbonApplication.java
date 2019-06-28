df344b413379f3f289d7904695369f9c3b03a883
Administrator <Administrator@JQJLJDG6B4V3H09>
2019-06-28 09:40:46 +0800

    清渭无情极

--- sp07-hystrix/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
+++ sp07-hystrix/src/main/java/com/tedu/sp06/Sp06RibbonApplication.java
@@ -1,5 +1,5 @@
 package com.tedu.sp06;
-//WWQQQQQQQQQQQ
+//WWQQQQQQQQQQQ7
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
